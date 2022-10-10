ac30ee384bbf380ee66b514a9f8af9a196f05fa3
revgeo <114108515+revgeo@users.noreply.github.com>
2022-10-10 16:08:56 -0500

    Add files via upload

---
+++ abv2kCals/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace abv2kcals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Value (ABV)  : ");
+            double abv = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input Value (Ounces)  : ");
+            double ounces = Convert.ToDouble(Console.ReadLine());
+
+            double kcals = abv*2.5*ounces; 
+
+            Console.WriteLine("There are approxiamtely {0} kcals in {1} ounces of a {2} ABV Beer", kcals, ounces, abv);
+            Console.ReadKey();
+        }
+    }
+}
