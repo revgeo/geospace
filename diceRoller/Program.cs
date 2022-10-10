18ec9a87639f1d73b73f51eb2b5c5063dbd8a6c5
revgeo <114108515+revgeo@users.noreply.github.com>
2022-10-10 16:10:38 -0500

    Add files via upload

---
+++ diceRoller/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace abv2kcals
+{
+	class Program
+	{
+		static void Main(string[] args)
+/*
+		{
+			int guess = 0;
+
+			do 
+			{
+				Console.Write("Input 0 for Heads or 1 for Tails: ");
+				guess = Convert.ToInt32(Console.ReadLine());
+				if (guess != 0 && guess != 1)
+				{
+					Console.WriteLine("Please enter a guess of 0 or 1");
+				}
+			} 
+			while (guess != 0 && guess != 1);
+		}
+*/
+		{
+      		string firstName = "John ";
+      		string lastName = "Doe";
+      		string name = string.Concat(firstName, lastName);
+      		Console.WriteLine(name);
+    	}
+	}
+}
